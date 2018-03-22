c89fed1c4ed3cc7eb3222d5a7dc5197b3641d310
micxye <michaelyeahh@gmail.com>
2018-03-21 20:03:03 -0700

    Working on click on title to display player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,41 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeVideo = this.changeVideo.bind(this);
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar" id="navbar">
+          <Search/>
+        </nav>
+        <div className="row">
+          <div className="col-md-7" id="videoplayer">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+          <div className="col-md-5" id="videolist">
+            <VideoList videos={this.state.videos}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+  changeVideo(props) {
+    this.setState(state => ({
+        currentVideo: props.video
+      }));
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+//ReactDOM.render(<Search/>, document.getElementById("navbar"));
+//ReactDOM.render(<VideoList/>, document.getElementById("videolist"));
+//ReactDOM.render(<VideoPlayer/>, document.getElementById("videoplayer"));
